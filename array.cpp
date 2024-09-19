4d8dbe30912a2b146918dd879eeb51bd4681550d
MANISH KUMAR 🕶️ <131944640+manishkumar802@users.noreply.github.com>
2024-09-19 19:01:10 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int marks[5] = {99,100,50,30 ,10};
+int size=5;
+double price[]= {10.33,44.33,33.33};
+cout<<marks[3]<<endl;
+cout<<sizeof(marks)<<endl;
+for(int i =0;i<size;i++){
+    cout<<marks[i]<<endl;
+}
+
+}
---
+++ reverse_array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void reverseArray(int arr[] ,int sz ){
+    int start=0,end=sz-1;
+    while(start<end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+
+}
+}
+int main(){
+int arr[]={4,5,6,7,8,9,10};
+int sz=7;
+reverseArray(arr,sz);
+    for(int i=0;i<sz;i++){
+        cout<<arr[i]<<" ";
+    }
+
+cout<<endl;
+}
